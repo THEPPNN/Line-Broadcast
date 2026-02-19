84157b5b5411bd50da05c9b44e4a532aeb370ee0
Parichat Pinitchote <theppnn3310@gmail.com>
2026-02-19 10:41:26 +0700

    Enhance announcement command scheduling to prevent overlapping executions and ensure single server execution

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('announcement:send')->everyMinute();
+Schedule::command('announcement:send')->everyMinute()
+    ->withoutOverlapping()
+    ->onOneServer();
